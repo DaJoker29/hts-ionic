c4b2563725ae61971cd6f95de1422b14f7e64340
Dewitt Buckingham <dj729@comcast.net>
2015-08-16 00:46:44 -0700

    Authentication
    * Integrate social authentication

--- www/js/app.js
+++ www/js/app.js
@@ -18,10 +18,37 @@ angular.module('hts', ['ionic', 'firebase'])
   });
 })
 
+.factory('Auth', function($firebaseAuth) {
+  var usersRef = new Firebase('https://harrietthespy.firebaseio.com/users');
+  return $firebaseAuth(usersRef);
+})
+
 .controller('AppCtrl', function() {
 
 })
 
-.controller('LoginCtrl', function() {
+.controller('LoginCtrl', function( $scope, Auth ) {
+
+  $scope.login = function( provider ) {
+    console.log("Trying ", provider);
+    Auth.$authWithOAuthRedirect( provider ).then(function (authData) {}).catch(function (error) {
+      if(error.code === 'TRANSPORT_UNAVAILABLE') {
+        Auth.$authWithOAuthPopup( provider).then(function(authData) {});
+      } else {
+        console.log(error);
+      }
+    });
+  };
+
+  Auth.$onAuth(function(authData) {
+    if (authData === null) {
+      console.log("Not logged in kid");
+    } else {
+      console.log('Logged in as', authData.id);
+    }
+
+    $scope.authData = authData;
+  });
+
 
 })

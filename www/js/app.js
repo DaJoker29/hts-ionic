7e8e2c60419aa91f183bd1103d607d8e21e44ccc
Dewitt Buckingham <dj729@comcast.net>
2015-08-15 22:56:27 -0700

    Login Buttons
    * Create login buttons
    * Add AngularFire
    * Create AngularControllers

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('hts', ['ionic'])
+angular.module('hts', ['ionic', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,4 +16,12 @@ angular.module('hts', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-});
+})
+
+.controller('AppCtrl', function() {
+
+})
+
+.controller('LoginCtrl', function() {
+
+})
